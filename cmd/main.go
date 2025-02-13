6b6a86b3ccb85347ce5c3fbd3f418c4f2c46e5ae
alemelis <alessandro_melis@rocketmail.com>
2025-02-13 11:31:36 +0000

    filini server

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,12 @@ package main
 
 import (
     "log"
-    "net/http"
-    "github.com/gin-gonic/gin"
-    // "github.com/alemelis/filini/server"
+    "github.com/alemelis/filini/server"
 )
 
 func main() {
-    r := gin.Default()
-
-    // Define routes
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"message": "Filini is alive!"})
-    })
-
-    // Start server
-    log.Println("Starting server on :8080...")
-    if err := r.Run(":8080"); err != nil {
-        log.Fatal(err)
-    }
+    server.InitDB()
+    log.Println("Starting filini...")
+    server.Start()
 }
+
--- server/server.go
+++ server/server.go
@@ -1,7 +1,54 @@
 package server
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+    "net/http"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+    "github.com/gin-gonic/gin"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "postgres://filini:password@localhost:5432/filini?sslmode=disable"
+	}
+
+	var err error
+	DB, err = sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Database is unreacheable:", err)
+	}
+
+	fmt.Println("Connected to PostgreSQL!")
+}
 
 func Init() {
-    fmt.Println("Server package initialised")
+	fmt.Println("Server package initialised")
+}
+
+func Start() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+
+    router := gin.Default()
+
+	// Define routes
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Filini is running!"})
+	})
+
+	fmt.Println("Filini is running on port", port)
+	log.Fatal(router.Run(":" + port)) // Start the server
+
 }
